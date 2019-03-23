99f42ec547b488fb9bade0fe2bb916643d323290
Giancarlo Vera <gianverab@gmail.com>
2019-03-23 09:35:15 -0500

    add skills component and setup

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,3 +1,16 @@
 import React from 'react';
 
+const Skills = () => (
+  <div className="Skills">
+    <div className="Skills-container">
+      <div className="Skills-item">
+        <h5>HTML</h5>
+        <div className="Skills-bar">
+          <span>95%</span>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
 export default Skills;
