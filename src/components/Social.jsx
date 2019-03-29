912bbf89a332147b04a5d5a3219943ab1509b82d
Giancarlo Vera <gianverab@gmail.com>
2019-03-28 23:31:29 -0500

    using themeprovider from styled components

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -1,19 +1,71 @@
 import React from 'react';
+import styled, { ThemeProvider } from 'styled-components';
+
+const SocialStyle = styled.div`
+  display: block;
+  margin: 0 auto;
+`;
+
+const SocialList = styled.ul`
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+`;
+
+const SocialItem = styled.li`
+  display: inline;
+  margin: 0 0.5em 0 0;
+`;
+
+const SocialAnchor = styled.a`
+  color: #212121;
+  text-decoration: none;
+  font-size: 1.2em;
+`;
+
+const SocialIcon = styled.i`
+  color: ${props => props.theme.color};
+`;
+
+const facebook = {
+  color: '#3b5998',
+};
+
+const twitter = {
+  color: '#38a1f3',
+};
+
+const linkedin = {
+  color: '#0e76a8',
+};
+
+const github = {
+  color: '#333',
+};
+
+const getColor = name => {
+  if (name === 'facebook') return facebook;
+  if (name === 'twitter') return twitter;
+  if (name === 'linkedin') return linkedin;
+  if (name === 'github') return github;
+};
 
 const Social = props => (
-  <div className="Social">
+  <SocialStyle>
     {props.data && (
-      <ul>
+      <SocialList>
         {props.data.map((social, index) => (
-          <li className="Social-item" key={`Social-${index}`}>
-            <a href={social.url} target="_blank">
-              {social.name}
-            </a>
-          </li>
+          <SocialItem className="Social-item" key={`Social-${index}`}>
+            <SocialAnchor href={social.url} target="_blank">
+              <ThemeProvider theme={getColor(social.name)}>
+                <SocialIcon className={`fab fa-${social.name}`} />
+              </ThemeProvider>
+            </SocialAnchor>
+          </SocialItem>
         ))}
-      </ul>
+      </SocialList>
     )}
-  </div>
+  </SocialStyle>
 );
 
 export default Social;
