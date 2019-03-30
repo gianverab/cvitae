fe6917e7259205885d81b8b1fcfaf1e7693aeba6
Giancarlo Vera <gianverab@gmail.com>
2019-03-29 23:12:20 -0500

    create the skills component

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,17 +1,47 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const SkillsContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 2rem;
+  grid-row-gap: 0.5em;
+`;
+
+const SkillsTitle = styled.h5`
+  margin: 0.5em 0;
+`;
+
+const SkillsBar = styled.div`
+  height: 8px;
+  position: relative;
+  border-radius: 50px;
+  background: #f8bbd0;
+`;
+
+const SkillsSpan = styled.span`
+  display: block;
+  height: 100%;
+  width: ${props => props.width};
+  border-radius: 8px;
+  background-color: #c2185b;
+  overflow: hidden;
+  position: relative;
+  box-shadow: inset 0 2px 9px rgba(255, 255, 255, 0.3), inset 0 -2px 6px rgba(0, 0, 0, 0.4);
+`;
 
 const Skills = props => (
   <div className="Skills">
-    <div className="Skills-container">
+    <SkillsContainer>
       {props.data.map((skill, index) => (
         <div className="Skills-item" key={`Skill-${index}`}>
-          <h5>{skill.name}</h5>
-          <div className="Skills-bar">
-            <span>{skill.percentage}</span>
-          </div>
+          <SkillsTitle>{skill.name}</SkillsTitle>
+          <SkillsBar>
+            <SkillsSpan width={skill.percentage} />
+          </SkillsBar>
         </div>
       ))}
-    </div>
+    </SkillsContainer>
   </div>
 );
 
