d0d1f97c71411059b6329d050b45514650f7c781
Giancarlo Vera <gianverab@gmail.com>
2019-03-30 10:31:00 -0500

    global styles with styled components

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const MainStyled = styled.div`
+  display: grid;
+  grid-template-columns: minmax(auto, 1024px);
+  justify-content: center;
+  padding: 1em;
+`;
+
+const MainContainer = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 3fr;
+  grid-gap: 2rem;
+  grid-row-gap: 0.5em;
+`;
 
 const Main = ({ children }) => (
-  <div className="Main">
-    <div className="Main-container">{children}</div>
-  </div>
+  <MainStyled>
+    <MainContainer>{children}</MainContainer>
+  </MainStyled>
 );
 
 export default Main;
